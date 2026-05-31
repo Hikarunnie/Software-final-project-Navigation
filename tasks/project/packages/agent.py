833e89bbc08f6f85215e9ad554abb11b45869b21
Hikarunnie <Kharabadze.Ani@kiu.edu.ge>
2026-05-31 17:36:34 +0400

    Added Dijkstra for optimal path computation

--- tasks/project/packages/agent.py
+++ tasks/project/packages/agent.py
@@ -1,9 +1,10 @@
 import time
 from road_map import road_map
 import servers.project.virtual_server as server
+from tasks.project.packages.optimal_path import dijkstra
 
 def main(camera, wheels, leds, stop_event):
-
+    print("AGENT FILE LOADED")
     print(f"[Agent] Start: {server.current_node}, Goal: {server.goal_node}")
     print(f"[Agent] Neighbors of start: {road_map.neighbors(server.current_node)}")
     print(f"[Agent] Shortest edge from {server.current_node} to {server.goal_node}: {road_map.shortest_edge(server.current_node, server.goal_node)}")
@@ -19,8 +20,14 @@ def main(camera, wheels, leds, stop_event):
                 time.sleep(0.02)
                 continue
 
-            # pathfinding code will go here,
-            # using start and goal which update live from the UI
+            route = dijkstra(start, goal)
+
+            print(f"[PathPlanner] start={start}, goal={goal}")
+            print(f"[PathPlanner] path={route['path']}")
+            print(f"[PathPlanner] edges={route['edges']}")
+            print(f"[PathPlanner] distance={route['distance']}")
+
+            time.sleep(1)
 
     finally:
         wheels.set_wheels_speed(0.0, 0.0)
